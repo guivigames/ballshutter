ec2496bd990dd8dfe6648831929551c3b1495074
guivigames <games@guivi.one>
2021-08-15 13:02:57 +0100

    Collition detection and spacebar for starting game

--- one.cpp
+++ one.cpp
@@ -35,6 +35,31 @@ bool isOutX( Bullet b)
     return false;
 }
 
+// CIRCLE/RECTANGLE
+bool circleRect(float cx, float cy, float rad, float rx, float ry, float rw, float rh) {
+
+  // temporary variables to set edges for testing
+  float testX = cx;
+  float testY = cy;
+
+  // which edge is closest?
+  if (cx < rx)         testX = rx;      // test left edge
+  else if (cx > rx+rw) testX = rx+rw;   // right edge
+  if (cy < ry)         testY = ry;      // top edge
+  else if (cy > ry+rh) testY = ry+rh;   // bottom edge
+
+  // get distance from closest edges
+  float distX = cx-testX;
+  float distY = cy-testY;
+  float distance = sqrt( (distX*distX) + (distY*distY) );
+
+  // if the distance is less than the radius, collision!
+  if (distance <= rad) {
+    return true;
+  }
+  return false;
+}
+
 int main()
 {
     srand(time(NULL));
@@ -46,59 +71,86 @@ int main()
     shape.setFillColor(sf::Color::Green);
     shape.setPosition(  pos);
 
+    bool gameover = true;
+
     sf::Clock timer;
     while (window.isOpen())
     {
-        
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
+        if (!gameover){
+            sf::Event event;
+            while (window.pollEvent(event))
+            {
+                if (event.type == sf::Event::Closed)
+                    window.close();
+            }
+            
+            sf::Time dt = timer.restart();
+            sf::Vector2f newpos = pos;
+
+            newpos.y -= (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))      ?2 * 100 * dt.asSeconds():0;
+            newpos.y += (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))    ?2 * 100 * dt.asSeconds():0;
+            newpos.x -= (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))    ?2 * 100 * dt.asSeconds():0;
+            newpos.x += (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))   ?2 * 100 * dt.asSeconds():0;
+
+            for (auto& b : bullets)
+                b.pos.x-= b.speed * 100 * dt.asSeconds();
+
+            for (auto& b : bullets)
+            {
+                if (circleRect(newpos.x+radius, newpos.y+radius, radius, b.pos.x, b.pos.y, bwidth, bheight))
+                    gameover = true;//return true;
+            }
+            std::remove_if(bullets.begin(), bullets.end(), isOutX);
+
+            if(rand()% 100 > 98)
+                bullets.push_back( createBullet());
+
+            if (newpos.x >= 0 && newpos.x <= width-radius * 2)
+                pos.x = newpos.x;
+            if (newpos.y > 0 && newpos.y <= height - radius * 2)
+                pos.y = newpos.y;
+
+            shape.setPosition(  pos);
+
+
+            window.clear();
+            for (auto& b : bullets)
+            {
+                sf::RectangleShape r;
+                r.setFillColor(sf::Color::Red);
+                r.setPosition(b.pos);
+                r.setSize(sf::Vector2f(bwidth, bheight));
+                window.draw(r);
+            }
+            window.draw(shape);
+            window.display();
         }
-        
-        sf::Time dt = timer.restart();
-        sf::Vector2f newpos = pos;
-
-        newpos.y -= (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))      ?2 * 100 * dt.asSeconds():0;
-        newpos.y += (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))    ?2 * 100 * dt.asSeconds():0;
-        newpos.x -= (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))    ?2 * 100 * dt.asSeconds():0;
-        newpos.x += (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))   ?2 * 100 * dt.asSeconds():0;
-
-        for (auto& b : bullets)
-            b.pos.x-= b.speed * 100 * dt.asSeconds();
-
-        for (auto& b : bullets)
-        {
-            float dist = pow ( pow(b.pos.x - newpos.x, 2)+pow(b.pos.y - newpos.y,2), 0.5);
-            if (dist < radius)  // Gameover.
-                return 0;
+        else {
+            sf::Event event;
+            while (window.pollEvent(event))
+            {
+                if (event.type == sf::Event::Closed)
+                    window.close();
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
+            {
+                pos.x = 20;
+                pos.y = height / 2;
+                bullets.clear();
+                gameover = false;
+            }
+            window.clear();
+            for (auto& b : bullets)
+            {
+                sf::RectangleShape r;
+                r.setFillColor(sf::Color::Red);
+                r.setPosition(b.pos);
+                r.setSize(sf::Vector2f(bwidth, bheight));
+                window.draw(r);
+            }
+            window.draw(shape);
+            window.display();
         }
-        
-        std::remove_if(bullets.begin(), bullets.end(), isOutX);
-
-        if(rand()% 100 > 98)
-            bullets.push_back( createBullet());
-
-        if (newpos.x >= 0 && newpos.x <= 780)
-            pos.x = newpos.x;
-        if (newpos.y > 0 && newpos.y <= 580)
-            pos.y = newpos.y;
-
-        shape.setPosition(  pos);
-
-
-        window.clear();
-        for (auto& b : bullets)
-        {
-            sf::RectangleShape r;
-            r.setFillColor(sf::Color::Red);
-            r.setPosition(b.pos);
-            r.setSize(sf::Vector2f(bwidth, bheight));
-            window.draw(r);
-        }
-        window.draw(shape);
-        window.display();
         
     }
 
